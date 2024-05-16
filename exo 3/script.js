5d13d6632b482e5f36116f68f180e2cb8acd1407
Nabil <l4teoyt@gmail.com>
2024-05-16 15:07:47 +0200

    script de l'exo 3 terminé

--- exo 3/script.js
+++ exo 3/script.js
@@ -1,13 +1,32 @@
 const boxes = document.querySelectorAll(".box")
-
-
+const txt = document.getElementById("txt")
+let boolean = false
+let compteur = 0
 
 boxes.forEach((box) => {
     box.addEventListener('click', function() {
-        box.textContent = "X";
-    })
+
+        compteur++
+
+        if (boolean == true){
+            box.textContent = "O"
+            box.style.color ="green"
+            boolean = false
+        }
+        else {
+            box.textContent = "X";
+            box.style.color ="red"
+            boolean = true
+        } 
+
+        if (compteur == 9) {
+            txt.textContent = "Partie terminée"
+        } else if (compteur == 2 || compteur == 4 || compteur == 6 || compteur == 8){
+            txt.textContent = "C'est au tour de X"
+        } else {
+            txt.textContent = "C'est au tour de O"
+        }
+    },{once:true})
+    
 })
-// faire un if sur un booleen
-// si true quand on clique inner html avec O
-// si false avec X
-// a chaque clique ca change le booleen, + change la couleur du txt
+
